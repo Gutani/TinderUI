dcd91d0b5f4c719f0ab07629c04883d3c7c96b70
Gustavo Antunes <gustavoamalheiros@gmail.com>
2022-02-15 07:39:49 -0300

    Initial Commit

---
+++ TinderUI/TinderUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderUIApp.swift
+//  TinderUI
+//
+//  Created by Gustavo Malheiros on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TinderUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
